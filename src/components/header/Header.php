332573485672c9dc227ea130c25cbe95c75b61a2
AYOJWANNIARACHCHI <ayojwanniarachchi22@gmail.com>
2023-08-28 23:48:32 +0530

    Header Done

---
+++ src/components/header/Header.php
@@ -0,0 +1,41 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="../../styles/header.css">
+</head>
+
+<body>
+    <!-- Navigation menu-->
+    <nav>
+        <div class="logo">
+            <a href="http://"><img src="" alt="" />
+                <span>Selling Fast</span>
+            </a>
+        </div>
+        <div class="navLinks">
+            <ul>
+                <li><a href="">All Ads</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="reg-log-buttons">
+            <button class="signin">SignIn</button>
+            <button class="signup">Register</button>
+        </div>
+    </nav>
+
+    <section class="header-section">
+        <div class="caption">
+            <span>The Best</br> Property Selling </br>Platform</span>
+        </div>
+        <div class="header-section-image">
+            <img src="../../images/HomePagePic.png" alt="" srcset="">
+        </div>
+    </section>
+</body>
+
+</html>
